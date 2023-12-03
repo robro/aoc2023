1d0615fa96ddbfe06cb1a9af47b62b8f12041a81
Ian Brock <pibborando@gmail.com>
2023-12-02 21:18:58 -0800

    🔧 Refactor day 2

--- day2/day2.gd
+++ day2/day2.gd
@@ -2,72 +2,63 @@ extends Node2D
 
 #@onready var input_lines := FileAccess.open("res://day2/example_1.txt", FileAccess.READ).get_as_text().split("\n")
 @onready var input_lines := FileAccess.open("res://day2/input.txt", FileAccess.READ).get_as_text().split("\n")
-@onready var regex := RegEx.new()
 
 func _ready():
   print("Day 2")
   part_one(input_lines)
   part_two(input_lines)
 
-func part_one(games:PackedStringArray):
+func part_one(lines:PackedStringArray):
   var id_sum := 0
-  var limits := {
-    "red": 12,
-    "green": 13,
-    "blue": 14
-  }
-  var re_array := [r"Game (?<game>\d+)"]
-  for color in limits:
-    re_array.append(r"(?<{c}>\d+) {c}".format({"c": color}))
-  regex.compile(r"|".join(re_array))
+  var limits := {"red": 12, "green": 13, "blue": 14}
+  var regex := RegEx.new()
+  regex.compile(r"Game (?<game>\d+)")
   
-  for game in games:
-    var matches := regex.search_all(game)
-    if matches.is_empty():
-      continue
-      
+  for line in Array(lines).filter(func(x): return x != ""):
+    var max_values := get_max_values(line, limits.keys())
     var is_possible := true
-    var game_id:int
-    
-    for m in matches:
-      var id_str := m.get_string("game")
-      if !id_str.is_empty():
-        game_id = int(id_str)
-      for limit in limits:
-        var value := m.get_string(limit)
-        if !value.is_empty() and int(value) > limits[limit]:
-          is_possible = false
-          break
-          
+    var game_id := int(regex.search(line).get_string("game"))
+
+    for color in limits:
+      if max_values[color] > limits[color]:
+        is_possible = false
+        break
     if is_possible:
       id_sum += game_id
       
   print("Part One: ", id_sum)
   
-func part_two(games:PackedStringArray):
+func part_two(lines:PackedStringArray):
   var power_sum := 0
-  var values := {"red": 0, "green": 0, "blue": 0}
-  var re_array := []
-  for color in values:
-    re_array.append(r"(?<{c}>\d+) {c}".format({"c": color}))
-  regex.compile(r"|".join(re_array))
+  var colors := ["red", "green", "blue"]
   
-  for game in games:
-    for key in values:
-      values[key] = 0
-    var matches := regex.search_all(game)
-    if matches.is_empty():
-      continue
-    
-    for m in matches:
-      for color in values:
-        var value := m.get_string(color)
-        if !value.is_empty() and int(value) > values[color]:
-          values[color] = int(value)
-          
+  for line in Array(lines).filter(func(x): return x != ""):
+    var max_values := get_max_values(line, colors)
     var power := 1
-    for v in values.values():
+    for v in max_values.values():
       power *= v
     power_sum += power
     
   print("Part Two: ", power_sum)
+  
+func get_max_values(line:String, colors:Array) -> Dictionary:
+  var regex := RegEx.new()
+  var re_array := []
+  for color in colors:
+    re_array.append(r"(?<{c}>\d+) {c}".format({"c": color}))
+  regex.compile(r"|".join(re_array))
+  
+  var max_values := {}
+  for color in colors:
+    max_values[color] = 0
+  var matches := regex.search_all(line)
+  if matches.is_empty():
+    return max_values
+      
+  for m in matches:
+    for color in colors:
+      var value := m.get_string(color)
+      if !value.is_empty() and int(value) > max_values[color]:
+        max_values[color] = int(value)
+
+  return max_values
