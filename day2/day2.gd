bc8018e6976a5dc02cb0b9ac8c4c6dd8524b3f27
Ian Brock <pibborando@gmail.com>
2023-12-02 17:20:09 -0800

    ✨ Add day 2 solution

---
+++ day2/day2.gd
@@ -0,0 +1,67 @@
+extends Node2D
+
+#@onready var input_lines := FileAccess.open("res://day2/example_1.txt", FileAccess.READ).get_as_text().split("\n")
+@onready var input_lines := FileAccess.open("res://day2/input.txt", FileAccess.READ).get_as_text().split("\n")
+@onready var regex := RegEx.new()
+
+func _ready():
+  print("Day 2")
+  part_one(input_lines)
+  part_two(input_lines)
+
+func part_one(games:PackedStringArray):
+  var limits := {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+  }
+  var id_sum := 0
+  regex.compile(r"Game (?<game>\d+)|(?<red>\d+) red|(?<green>\d+) green|(?<blue>\d+) blue")
+  
+  for game in games:
+    var matches := regex.search_all(game)
+    if matches.is_empty():
+      continue
+      
+    var is_possible := true
+    var game_id:int
+    
+    for m in matches:
+      var id_str := m.get_string("game")
+      if id_str:
+        game_id = int(id_str)
+      for limit in limits:
+        var value := m.get_string(limit)
+        if !value.is_empty() and int(value) > limits[limit]:
+          is_possible = false
+          break
+          
+    if is_possible:
+      id_sum += game_id
+      
+  print("Part One: ", id_sum)
+  
+func part_two(games:PackedStringArray):
+  var values := {"red": 0, "green": 0, "blue": 0}
+  var power_sum := 0
+  regex.compile(r"(?<red>\d+) red|(?<green>\d+) green|(?<blue>\d+) blue")
+  
+  for game in games:
+    for key in values:
+      values[key] = 0
+    var matches := regex.search_all(game)
+    if matches.is_empty():
+      continue
+    
+    for m in matches:
+      for color in values:
+        var value := m.get_string(color)
+        if !value.is_empty() and int(value) > values[color]:
+          values[color] = int(value)
+          
+    var power := 1
+    for v in values.values():
+      power *= v
+    power_sum += power
+    
+  print("Part Two: ", power_sum)
