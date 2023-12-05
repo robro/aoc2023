de1181cec24916213da128dd2844b8acb8410627
Ian Brock <pibborando@gmail.com>
2023-12-04 16:50:16 -0800

    ✨ Add day 4 part 2 solution and refactor

--- day4/day4.gd
+++ day4/day4.gd
@@ -4,33 +4,21 @@ extends Node2D
 @onready var input_lines := FileAccess.open("res://day4/input.txt", FileAccess.READ).get_as_text().split("\n")
 
 class Scratchcard:
-  var winning_nums := []
-  var your_nums := []
+  var match_count := 0
   
   func _init(winning_nums:Array, your_nums:Array):
-    for num in winning_nums:
-      self.winning_nums.append(int(num))
-    for num in your_nums:
-      self.your_nums.append(int(num))
+    for y_num in your_nums:
+      for w_num in winning_nums:
+        if y_num == w_num:
+          self.match_count += 1
     
   func get_point_value():
-    var matches := 0
-    for y_num in self.your_nums:
-      for w_num in self.winning_nums:
-        if y_num == w_num:
-          matches += 1
-          
-    return 0 if matches == 0 else pow(2, matches-1)
-
-func _ready():
-  print("Day 4")
-  part_one(Array(input_lines).filter(func(x): return x != ""))
-
-func part_one(lines:Array):
-  var total_points := 0
+    return 0 if self.match_count == 0 else pow(2, self.match_count-1)
+    
+func get_cards(lines:Array) -> Array[Scratchcard]:
   var cards:Array[Scratchcard] = []
   var regex := RegEx.new()
-  regex.compile(r": (?<win>.+) \| (?<you>.+)$")
+  regex.compile(r"Card \s*(?<num>\d+): (?<win>.+) \| (?<you>.+)$")
   
   for line in lines:
     var numbers := regex.search(line)
@@ -39,7 +27,34 @@ func part_one(lines:Array):
       numbers.get_string("you").split(" ", false)
     ))
     
+  return cards
+
+func _ready():
+  print("Day 4")
+  var cards := get_cards(Array(input_lines).filter(func(x): return x != ""))
+  part_one(cards)
+  part_two(cards)
+
+func part_one(cards:Array[Scratchcard]):
+  var total_points := 0
   for card in cards:
     total_points += card.get_point_value()
     
   print("Part One: ", total_points)
+
+func part_two(cards:Array[Scratchcard]):
+  var card_count := cards.size()
+  var total := 0
+  var totals := {}
+  for i in range(card_count):
+    totals[i] = 1
+    
+  for i in range(card_count):
+    for j in range(i+1, i+cards[i].match_count+1):
+      if j >= card_count:
+        break
+      totals[j] += totals[i]
+      
+    total += totals[i]
+    
+  print("Part Two: ", total)
