bc296fd88101da5612e927d79c8cf01cc737eaff
Ian Brock <pibborando@gmail.com>
2023-12-07 01:32:09 -0800

    ✨ Add day 7 part 2 solution

--- day7/day7.gd
+++ day7/day7.gd
@@ -6,23 +6,50 @@ extends Node2D
 class Hand:
   var cards := ""
   var bid := 0
-  var labels := ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+  var types := [[], [2], [2, 2], [3], [2, 3], [4], [5]]
   
   func _init(cards:String, bid:int):
     self.cards = cards
     self.bid = bid
     
   func get_strength() -> int:
-    var types := [[], [2], [2, 2], [3], [2, 3], [4], [5]]
     var counts := []
+    var labels := []
     
-    for label in self.labels:
+    for label in self.cards:
+      if label in labels:
+        continue
       var count := self.cards.count(label)
       if count > 1:
         counts.append(count)
+        labels.append(label)
       
     counts.sort()
-    return types.find(counts)
+    return self.types.find(counts)
+    
+  func get_strength_j() -> int:
+    var counts := []
+    var labels := []
+    var jokers := 0
+    
+    for label in self.cards:
+      if label in labels:
+        continue
+      var count := self.cards.count(label)
+      if label == "J":
+        jokers += count
+        labels.append(label)
+      elif count > 1:
+        counts.append(count)
+        labels.append(label)
+      
+    counts.sort()
+    if counts == []:
+      if jokers > 0:
+        counts.append(min(5, jokers + 1))
+    else:
+      counts[-1] += jokers
+    return self.types.find(counts)
     
   func _to_string() -> String:
     return "(Cards: {c}, Bid: {b})".format({"c": self.cards, "b": self.bid})
@@ -30,6 +57,7 @@ class Hand:
 func _ready():
   print("Day 7")
   part_one(input_lines)
+  part_two(input_lines)
   
 func part_one(input_lines:PackedStringArray):
   var total := 0
@@ -46,8 +74,25 @@ func part_one(input_lines:PackedStringArray):
     total += hands[i].bid * (i + 1)
     
   print("Part One: ", total)
+  
+func part_two(input_lines:PackedStringArray):
+  var total := 0
+  var regex := RegEx.new()
+  regex.compile(r"(?<cards>\S+)\s+(?<bid>\S+)")
+  var hands:Array[Hand] = []
+  
+  for line in Array(input_lines).filter(func(x): return x != ""):
+    var search = regex.search(line)
+    hands.append(Hand.new(search.get_string("cards"), int(search.get_string("bid"))))
+
+  hands.sort_custom(sort_strength_j)
+  for i in hands.size():
+    total += hands[i].bid * (i + 1)
+    
+  print("Part Two: ", total)
 
 func sort_strength(a:Hand, b:Hand) -> bool:
+  var labels := ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
   var a_strength := a.get_strength()
   var b_strength := b.get_strength()
   
@@ -57,8 +102,29 @@ func sort_strength(a:Hand, b:Hand) -> bool:
     return false
   if a_strength == b_strength:
     for i in a.cards.length():
-      var a_label_strength := a.labels.find(a.cards[i])
-      var b_label_strength := b.labels.find(b.cards[i])
+      var a_label_strength := labels.find(a.cards[i])
+      var b_label_strength := labels.find(b.cards[i])
+      
+      if a_label_strength < b_label_strength:
+        return true
+      if a_label_strength > b_label_strength:
+        return false
+        
+  return true
+  
+func sort_strength_j(a:Hand, b:Hand) -> bool:
+  var labels := ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
+  var a_strength := a.get_strength_j()
+  var b_strength := b.get_strength_j()
+  
+  if a_strength < b_strength:
+    return true
+  if a_strength > b_strength:
+    return false
+  if a_strength == b_strength:
+    for i in a.cards.length():
+      var a_label_strength := labels.find(a.cards[i])
+      var b_label_strength := labels.find(b.cards[i])
       
       if a_label_strength < b_label_strength:
         return true
