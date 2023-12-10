57f2b701a636650deaa6651c6973d50c5bbc97e1
Ian Brock <pibborando@gmail.com>
2023-12-10 11:37:29 -0800

    🔧 Refactor part 1 to be more object oriented

--- day10/day10.gd
+++ day10/day10.gd
@@ -6,17 +6,21 @@ extends Node2D
 
 class Pipe:
   var char := ""
+  var location := Vector2.ZERO
+  var steps := 0
   var map := {
     "|": "NS",
     "-": "EW",
     "L": "NE",
     "J": "NW",
     "7": "SW",
-    "F": "SE"
+    "F": "SE",
+    "S": "NSEW",
   }
   
-  func _init(char:String):
+  func _init(char:String, location:Vector2):
     self.char = char
+    self.location = location
     
   func get_offsets():
     return self.map[self.char]
@@ -28,42 +32,43 @@ func _ready():
 func part_one(input_lines:PackedStringArray):
   input_lines = Array(input_lines).filter(func(x): return x != "") as Array[String]
   var grid := []
-  var start_location := Vector2.ZERO
+  var start_pipe:Pipe
   
   for y in input_lines.size():
     var row := []
     for x in input_lines[y].length():
       var char = input_lines[y][x]
-      if char == "S":
-        start_location = Vector2(x, y)
-        row.append(char)
-      elif char == ".":
-        row.append(char)
+      if char == ".":
+        row.append(null)
       else:
-        row.append(Pipe.new(char))
+        var pipe := Pipe.new(char, Vector2(x, y))
+        if char == "S":
+          start_pipe = pipe
+        row.append(pipe)
         
     grid.append(row)
     
-  var locations_traveled := []
-  var locations_to_travel := [{"location": start_location, "steps": 0}]
+  var pipes_traveled:Array[Pipe] = []
+  var pipes_to_travel:Array[Pipe] = [start_pipe]
   var steps := 0
   
   while true:
-    var next_location = locations_to_travel.pop_front()
-    locations_traveled.append(next_location["location"])
-    var connecting_locs := get_connecting(grid, next_location["location"]).filter(
-      func(x): return x not in locations_traveled
+    var current_pipe:Pipe = pipes_to_travel.pop_front()
+    pipes_traveled.append(current_pipe)
+    var connecting_pipes := get_connecting_pipes(grid, current_pipe).filter(
+      func(x): return x not in pipes_traveled
     )
-    if connecting_locs.size() == 0:
-      steps = next_location["steps"]
+    if connecting_pipes.size() == 0:
+      steps = current_pipe.steps
       break
-    for loc in connecting_locs:
-      locations_to_travel.append({"location": loc, "steps": next_location["steps"] + 1})
+    for pipe in connecting_pipes:
+      pipe.steps = current_pipe.steps + 1
+      pipes_to_travel.append(pipe)
     
   print("Part One: ", steps)
   
-func get_connecting(grid:Array, location:Vector2) -> Array[Vector2]:
-  var connecting:Array[Vector2] = []
+func get_connecting_pipes(grid:Array, pipe:Pipe) -> Array[Pipe]:
+  var connecting:Array[Pipe] = []
   var offsets := {
     "N": Vector2(0, -1),
     "S": Vector2(0, 1),
@@ -77,23 +82,22 @@ func get_connecting(grid:Array, location:Vector2) -> Array[Vector2]:
     "W": "E"
   }
   var check_offsets := {}
-  var value = grid[location.y][location.x]
   
-  if value is Pipe:
-    for direction in value.get_offsets():
+  if pipe is Pipe:
+    for direction in pipe.get_offsets():
       check_offsets[direction] = offsets[direction]
   else:
     check_offsets = offsets
 
   for offset in check_offsets:
-    var check_loc:Vector2 = location + offsets[offset]
+    var check_loc:Vector2 = pipe.location + offsets[offset]
     if check_loc < Vector2.ZERO:
       continue
     if check_loc.x >= grid[0].size() || check_loc.y >= grid.size():
       continue
-    var check_val = grid[check_loc.y][check_loc.x]
-    if check_val is Pipe:
-      if connections[offset] in check_val.get_offsets():
-        connecting.append(check_loc)
+    var check_pipe = grid[check_loc.y][check_loc.x]
+    if check_pipe is Pipe:
+      if connections[offset] in check_pipe.get_offsets():
+        connecting.append(check_pipe)
 
   return connecting
