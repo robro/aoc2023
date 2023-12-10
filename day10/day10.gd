ba37c7f89b503358e2c56393cab745ee9d068cb2
Ian Brock <pibborando@gmail.com>
2023-12-10 11:54:27 -0800

    🔧 Move var declarations outside loops

--- day10/day10.gd
+++ day10/day10.gd
@@ -31,44 +31,49 @@ func _ready():
   
 func part_one(input_lines:PackedStringArray):
   input_lines = Array(input_lines).filter(func(x): return x != "") as Array[String]
+  var row:Array
+  var char:String
+  var pipe:Pipe
   var grid := []
   var start_pipe:Pipe
   
   for y in input_lines.size():
-    var row := []
+    row = []
     for x in input_lines[y].length():
-      var char = input_lines[y][x]
+      char = input_lines[y][x]
       if char == ".":
         row.append(null)
       else:
-        var pipe := Pipe.new(char, Vector2(x, y))
+        pipe = Pipe.new(char, Vector2(x, y))
         if char == "S":
           start_pipe = pipe
         row.append(pipe)
         
     grid.append(row)
-    
+  
+  var current_pipe:Pipe
+  var connecting_pipes:Array[Pipe]
   var pipes_traveled:Array[Pipe] = []
   var pipes_to_travel:Array[Pipe] = [start_pipe]
   var steps := 0
   
   while true:
-    var current_pipe:Pipe = pipes_to_travel.pop_front()
+    current_pipe = pipes_to_travel.pop_front()
     pipes_traveled.append(current_pipe)
-    var connecting_pipes := get_connecting_pipes(grid, current_pipe).filter(
+    connecting_pipes = get_connecting_pipes(grid, current_pipe).filter(
       func(x): return x not in pipes_traveled
     )
     if connecting_pipes.size() == 0:
       steps = current_pipe.steps
       break
-    for pipe in connecting_pipes:
-      pipe.steps = current_pipe.steps + 1
-      pipes_to_travel.append(pipe)
+    for connecting_pipe in connecting_pipes:
+      connecting_pipe.steps = current_pipe.steps + 1
+      pipes_to_travel.append(connecting_pipe)
     
   print("Part One: ", steps)
   
 func get_connecting_pipes(grid:Array, pipe:Pipe) -> Array[Pipe]:
-  var connecting:Array[Pipe] = []
+  var connecting_pipes:Array[Pipe] = []
   var offsets := {
     "N": Vector2(0, -1),
     "S": Vector2(0, 1),
@@ -82,22 +87,17 @@ func get_connecting_pipes(grid:Array, pipe:Pipe) -> Array[Pipe]:
     "W": "E"
   }
   var check_offsets := {}
+  for direction in pipe.get_offsets():
+    check_offsets[direction] = offsets[direction]
+    
+  var check_loc:Vector2
+  var check_pipe:Pipe
   
-  if pipe is Pipe:
-    for direction in pipe.get_offsets():
-      check_offsets[direction] = offsets[direction]
-  else:
-    check_offsets = offsets
-
   for offset in check_offsets:
-    var check_loc:Vector2 = pipe.location + offsets[offset]
-    if check_loc < Vector2.ZERO:
-      continue
-    if check_loc.x >= grid[0].size() || check_loc.y >= grid.size():
-      continue
-    var check_pipe = grid[check_loc.y][check_loc.x]
+    check_loc = pipe.location + check_offsets[offset]
+    check_pipe = grid[check_loc.y][check_loc.x]
     if check_pipe is Pipe:
       if connections[offset] in check_pipe.get_offsets():
-        connecting.append(check_pipe)
+        connecting_pipes.append(check_pipe)
 
-  return connecting
+  return connecting_pipes
